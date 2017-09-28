899f4810b9b87ce1cda1eb0fdd31d5d987ef9bd9
Marko Culinovic <marko.culinovic@gmail.com>
2017-09-28 21:33:02 +0200

    Added log folder for unit tests

--- Scripts/run_unit_tests.sh
+++ Scripts/run_unit_tests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#set log for database using GLOG environment variable
+mkdir unit_tests_logs
+GLOG_log_dir="./unit_tests_logs"
+
 ./build/KVStoreTests/bin/runTests
 # delete tmp db
 rm ./validateGet.db
